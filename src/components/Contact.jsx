153fb3055582c8be518c1941dba15ec1672915d8
Munish Dhiman <munishdhiman9@gmail.com>
2023-12-14 20:15:36 +0530

    Update Email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_apts7ba', // paste your ServiceID here (you'll get one when your service is created).
+        'template_h3mu0jq', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Munish Dhiman', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: 'munishd12@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        'nF8w7zegTAgVXoOAM' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
