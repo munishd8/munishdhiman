bf7a91b9487664d347ff81e423c0ac7991c4d26d
Munish Dhiman <munishdhiman9@gmail.com>
2025-05-20 17:51:48 +0530

    Update Project data

--- src/constants/index.js
+++ src/constants/index.js
@@ -161,116 +161,141 @@ const experiences = [
 const projects = [
   {
     id: 'project-1',
-    name: 'KomiKult',
-    description: 'A comic characters list app that displays Marvel characters.',
+    name: 'Vending Hive',
+    description: 'Vending Hive is a vending industry to connect, share insights, and manage operations. Built with ReactJS and Laravel, it features real-time chat via WebSockets and scalable Node.js microservices for efficient data handling and communication.',
     tags: [
-      {
-        name: 'react',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'mongodb',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'tailwind',
-        color: 'pink-text-gradient',
-      },
+      { name: 'Laravel', color: 'blue-text-gradient' },
+      { name: 'MYSQL', color: 'pink-text-gradient' },
+      { name: 'Web Socket', color: 'blue-text-gradient' },
+      { name: 'NodeJs Microservices', color: 'pink-text-gradient' },
     ],
     image: komikult,
-    repo: 'https://github.com/shaqdeff/KomiKult',
-    demo: 'https://shaqdeff.github.io/KomiKult/',
+    repo: '#',
+    demo: 'https://vendinghive.com/',  
   },
   {
     id: 'project-2',
-    name: 'Leaderboard',
-    description:
-      'A leaderboard list app that displays scores submitted by different players.',
+    name: 'Yolop',
+    description: 'Yolop is a real-time auction platform with a ReactJS frontend and a Laravel–Node.js microservice backend. It uses WebSockets for live bidding and millisecond-level cron jobs to sync data across web and mobile clients.',
     tags: [
-      {
-        name: 'react',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'restapi',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'scss',
-        color: 'pink-text-gradient',
-      },
+      { name: 'ReactJs', color: 'blue-text-gradient' },
+      { name: 'Laravel', color: 'green-text-gradient' },
+      { name: 'Web Socket', color: 'blue-text-gradient' },
+      { name: 'NodeJs Microservices', color: 'pink-text-gradient' },
+      { name: 'React Bootstrap', color: 'blue-text-gradient' },
     ],
-    image: leaderboard,
-    repo: 'https://github.com/shaqdeff/Leaderboard',
-    demo: 'https://shaqdeff.github.io/Leaderboard/',
+    image: komikult,
+    repo: '#',
+    demo: 'https://yolop.net/',
   },
   {
     id: 'project-3',
-    name: 'Math Magicians',
-    description: 'This is a single-page calculator app built with React',
+    name: 'PGTI',
+    description: 'The official PGTI website features a Laravel backend with a responsive front-end built using HTML, jQuery, and Bootstrap. It dynamically manages golf events, rankings, and player data, delivering a seamless and interactive user experience.',
     tags: [
-      {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'supabase',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'css',
-        color: 'pink-text-gradient',
-      },
+      { name: 'Laravel', color: 'blue-text-gradient' },
+      { name: 'MySQL', color: 'green-text-gradient' },
+      { name: 'Bootstrap', color: 'pink-text-gradient' },
     ],
-    image: math,
-    repo: 'https://github.com/shaqdeff/Math-Magicians',
-    demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+    image: komikult,
+    repo: '#',
+    demo: 'https://www.pgtofindia.com/',
   },
   {
     id: 'project-4',
-    name: 'Movie Metro',
-    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    name: 'Rozana Himachal',
+    description: 'Rozana Himachal is a multi-user news portal for Himachal, supporting multiple languages and a reward system to enhance engagement. SEO-friendly, with an HTML/jQuery frontend and a CodeIgniter backend for solid performance.',
     tags: [
-      {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'supabase',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'css',
-        color: 'pink-text-gradient',
-      },
+      { name: 'Codeigniter', color: 'blue-text-gradient' },
+      { name: 'MySQL', color: 'green-text-gradient' },
+      { name: 'HTML-CSS', color: 'pink-text-gradient' },
     ],
-    image: movie,
-    repo: 'https://github.com/shaqdeff/Movie-Metro',
-    demo: 'https://movie-metro.netlify.app/',
+    image: komikult,
+    repo: '#',
+    demo: 'https://rozanahimachal.com',
   },
   {
     id: 'project-5',
-    name: 'Nyeusi Fest Site',
-    description:
-      'This is a demo concert website for a music festival called Nyeusi.',
+    name: 'Cottage9',
+    description: 'Cottage9 is an eCommerce platform with a React-based front-end and Laravel-powered back-end, using MySQL for data storage and React Bootstrap for responsive UI.',
     tags: [
-      {
-        name: 'nextjs',
-        color: 'blue-text-gradient',
-      },
-      {
-        name: 'supabase',
-        color: 'green-text-gradient',
-      },
-      {
-        name: 'css',
-        color: 'pink-text-gradient',
-      },
+      { name: 'React', color: 'blue-text-gradient' },
+      { name: 'Laravel', color: 'green-text-gradient' },
+      { name: 'MySQL', color: 'pink-text-gradient' },
+      { name: 'React Bootstrap', color: 'blue-text-gradient' },
     ],
-    image: nyeusi,
-    repo: 'https://github.com/shaqdeff/Nyeusi-Fest-Site',
-    demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+    image: komikult,
+    repo: '#',
+    demo: 'https://www.cottage9.com/',
   },
 ];
+const projects2 = [
+  {
+    id: 'project-1',
+    name: 'Buy a Warranty',
+    description: 'A comic characters list app that displays Marvel characters.',
+    tags: [
+      { name: 'react', color: 'blue-text-gradient' },
+      { name: 'mongodb', color: 'green-text-gradient' },
+      { name: 'tailwind', color: 'pink-text-gradient' },
+    ],
+    image: komikult,
+    repo: '#',
+    demo: 'https://buyawarranty.co.uk/',
+  },
+  {
+    id: 'project-2',
+    name: 'Biznob',
+    description: 'Biznob information get by URL.',
+    tags: [
+      { name: 'react', color: 'blue-text-gradient' },
+      { name: 'mongodb', color: 'green-text-gradient' },
+      { name: 'tailwind', color: 'pink-text-gradient' },
+    ],
+    image: komikult,
+    repo: '#',
+    demo: 'https://www.biznob.com/',
+  },
+  {
+    id: 'project-3',
+    name: 'TBWE',
+    description: 'TBWE website data fetched from URL.',
+    tags: [
+      { name: 'react', color: 'blue-text-gradient' },
+      { name: 'mongodb', color: 'green-text-gradient' },
+      { name: 'tailwind', color: 'pink-text-gradient' },
+    ],
+    image: komikult,
+    repo: '#',
+    demo: 'https://tbwe.com/',
+  },
+  {
+    id: 'project-4',
+    name: 'Geevice',
+    description: 'Geevice platform showcase.',
+    tags: [
+      { name: 'react', color: 'blue-text-gradient' },
+      { name: 'mongodb', color: 'green-text-gradient' },
+      { name: 'tailwind', color: 'pink-text-gradient' },
+    ],
+    image: komikult,
+    repo: '#',
+    demo: 'https://geevice.com/',
+  },
+  {
+    id: 'project-5',
+    name: 'Rush Hour Daily',
+    description: 'Rush Hour Daily content platform.',
+    tags: [
+      { name: 'react', color: 'blue-text-gradient' },
+      { name: 'mongodb', color: 'green-text-gradient' },
+      { name: 'tailwind', color: 'pink-text-gradient' },
+    ],
+    image: komikult,
+    repo: '#',
+    demo: 'https://rushhourdaily.com/',
+  },
+];
+
 
 export { services, technologies, experiences, projects };
